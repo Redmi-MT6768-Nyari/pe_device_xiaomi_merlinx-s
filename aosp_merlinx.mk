072ecd8d8925e6fd340d01d7f4a0dc4f5f34e379
Zinadin Zidan <zidan.roking@gmail.com>
2022-02-20 17:50:23 +0300

    merlinx: Use pe recovery as default recovery
    Change-Id: Ia41cd5f05c1e2de5875111fdcaabc93ac9556a74

--- aosp_merlinx.mk
+++ aosp_merlinx.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/merlinx/device.mk)
 # Inherit some common PixelExperience stuff.
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_AOSP_RECOVERY := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_NAME := aosp_merlinx
